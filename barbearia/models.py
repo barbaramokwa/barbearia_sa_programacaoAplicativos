12961b230be5a58401c546efa72634f977f3bd33
BARBARA RANIE MOKWA <barbara_mokwa@estudante.sesisenai.org.br>
2026-09-15 11:28:26 -0300

    alterações

---
+++ barbearia/models.py
@@ -0,0 +1,16 @@
+class Agendamento:
+    def __init__(self, cliente, telefone, servico, preco, barbeiro, data, horario, status):
+        self.id = id
+        self.cliente = cliente
+        self.telefone = telefone
+        self.servico = servico
+        self.preco = preco
+        self.barbeiro = barbeiro
+        self.data = data
+        self.horario = horario
+        self.status = status
+
+    def exibir(self):
+        print(f"ID: {self.id}, Cliente: {self.cliente}, Telefone: {self.telefone}, Serviço: {self.servico}, Preço: R${self.preco}, Barbeiro: {self.barbeiro}, Data: {self.data}, Horario: {self.horario}, Status: {self.status}")
+
+    def converte_tupla(self):
